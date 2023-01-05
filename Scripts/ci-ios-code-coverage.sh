ac3b6e735a9b4ae4a1b47f637eb8795385e0bdcb
Toomas Vahter <toomas.vahter@gmail.com>
2023-01-05 21:08:08 +0200

    Use correct scheme when running tests on CI

--- Scripts/ci-ios-code-coverage.sh
+++ Scripts/ci-ios-code-coverage.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-SCHEME="IndexedDataStore"
+SCHEME="Stockroom"
 RESULT_BUNDLE="CodeCoverage.xcresult"
 RESULT_JSON="CodeCoverage.json"
-MIN_CODE_COVERAGE=50.0
+MIN_CODE_COVERAGE=10.0
 
 # Pre-clean
 if [ -d $RESULT_BUNDLE ]; then
@@ -14,10 +14,10 @@ if [ -f $RESULT_JSON ]; then
 fi
 
 # Build
-set -o pipefail && env NSUnbufferedIO=YES xcodebuild build-for-testing -scheme $SCHEME -destination "platform=iOS Simulator,OS=latest,name=iPhone 12" -enableCodeCoverage YES | xcpretty
+set -o pipefail && env NSUnbufferedIO=YES xcodebuild build-for-testing -scheme $SCHEME -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES | xcpretty
 
 # Test
-set -o pipefail && env NSUnbufferedIO=YES xcodebuild test-without-building -scheme $SCHEME -destination "platform=iOS Simulator,OS=latest,name=iPhone 12" -enableCodeCoverage YES -resultBundlePath $RESULT_BUNDLE | xcpretty
+set -o pipefail && env NSUnbufferedIO=YES xcodebuild test-without-building -scheme $SCHEME -destination "platform=iOS Simulator,OS=latest,name=iPhone 14" -enableCodeCoverage YES -resultBundlePath $RESULT_BUNDLE | xcpretty
 
 # Code coverage
 set -o pipefail && env NSUnbufferedIO=YES xcrun xccov view --report --json $RESULT_BUNDLE > $RESULT_JSON
